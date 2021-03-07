2ff4ff1d4b2a663e1f13c871fb57b358a1a9169a
spice2445 <79026145+spice2445@users.noreply.github.com>
2021-03-07 23:49:09 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+exports.min = function min(array) {
+  let g = Math.min.apply(null, array);
+  if (array == 0 || array == undefined) {
+    return 0;
+  }
+  return g;
+};
 
-exports.min = function min (array) {
-  return 0;
+exports.max = function max(array) {
+  let g = Math.max.apply(null, array);
+  if (array == 0 || array == undefined) {
+    return 0;
+  }
+  return g;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  let num = 0
+  if (array == 0 || array == undefined) {
+    return 0;
+  }
+  for (let i = 0; i < array.length; i++) {
+    num += array[i];
+  } 
+
+  let g = num / array.length;
+  return g;
+};
